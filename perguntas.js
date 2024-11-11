4eca4d7ca8b463b99d6faab19fa7cc302622f90c
AllanaCamargo <161711318+AllanaCamargo@users.noreply.github.com>
2024-11-11 09:47:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+   'Programaçao',
+    'O que é JavaScript?,
+    'O JavaScript é uma linguagm de Programação'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programaçao',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
